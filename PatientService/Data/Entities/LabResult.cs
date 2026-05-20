82d9facfd798f033066fe993eaa106053b486559
AtharvKishor <Atharv.Kishor@cognizant.com>
2026-05-20 17:35:50 +0530

    initial setup of project structure

---
+++ PatientService/Data/Entities/LabResult.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace PatientService.Data.Entities;
+
+[Index("SampleId", Name = "IX_LabResults_SampleId")]
+public partial class LabResult
+{
+    [Key]
+    public Guid ResultId { get; set; }
+
+    public Guid SampleId { get; set; }
+
+    public Guid RecordedByUserId { get; set; }
+
+    [StringLength(100)]
+    public string TestType { get; set; } = null!;
+
+    [StringLength(255)]
+    public string ResultValue { get; set; } = null!;
+
+    [Precision(0)]
+    public DateTime ResultDate { get; set; }
+
+    [ForeignKey("RecordedByUserId")]
+    [InverseProperty("LabResults")]
+    public virtual User RecordedByUser { get; set; } = null!;
+
+    [ForeignKey("SampleId")]
+    [InverseProperty("LabResults")]
+    public virtual Sample Sample { get; set; } = null!;
+}
---
+++ SampleService/Data/Entities/Sample.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace SampleService.Data.Entities;
+
+[Index("CollectedDate", Name = "IX_Samples_CollectedDate", AllDescending = true)]
+[Index("EnrollmentId", Name = "IX_Samples_EnrollmentId")]
+[Index("Status", Name = "IX_Samples_Status")]
+public partial class Sample
+{
+    [Key]
+    public Guid SampleId { get; set; }
+
+    public Guid EnrollmentId { get; set; }
+
+    public Guid CollectedByUserId { get; set; }
+
+    [StringLength(100)]
+    public string SampleType { get; set; } = null!;
+
+    [Precision(0)]
+    public DateTime CollectedDate { get; set; }
+
+    [StringLength(50)]
+    public string Status { get; set; } = null!;
+
+    [ForeignKey("CollectedByUserId")]
+    [InverseProperty("Samples")]
+    public virtual User CollectedByUser { get; set; } = null!;
+
+    [ForeignKey("EnrollmentId")]
+    [InverseProperty("Samples")]
+    public virtual PatientEnrollment Enrollment { get; set; } = null!;
+
+    [InverseProperty("Sample")]
+    public virtual ICollection<LabResult> LabResults { get; set; } = new List<LabResult>();
+}
