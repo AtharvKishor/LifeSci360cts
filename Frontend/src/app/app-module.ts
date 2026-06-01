694bcba6b2d62d962fd11885aeee058e3d8fb083
AtharvKishor <Atharv.Kishor@cognizant.com>
2026-06-01 09:10:14 +0530

    updated backend and did setup of frontend

--- Frontend/src/app/app-module.ts
+++ Frontend/src/app/app-module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { CommonModule, TitleCasePipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing-module';
-import { App, AppComponent } from './app';
+import { AppComponent } from './app';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    App,
+    AppComponent,
     LoginComponent,
     DashboardComponent
   ],
@@ -24,7 +24,8 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
     AppRoutingModule
   ],
   providers: [
-    provideHttpClient(withInterceptorsFromDi())
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
 })
